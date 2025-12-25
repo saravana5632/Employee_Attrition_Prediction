cb1ae4f5a138c2ded08227c6976c2ce29439c8f8
SaravanaKumar G <oatktg.saravana5632@gmail.com>
2025-12-25 13:05:23 +0530

    Updated Employee attrition dashboard Code

--- app.py
+++ app.py
@@ -5,38 +5,30 @@ import seaborn as sns
 import pickle
 from sklearn.preprocessing import LabelEncoder
 
-# -------------------- PAGE CONFIG --------------------
 st.set_page_config(
     page_title="Employee Attrition Dashboard",
     layout="wide"
 )
 
-# -------------------- LOAD DATA --------------------
 df = pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
-
-# Convert target column
 df['Attrition'] = df['Attrition'].map({'Yes': 1, 'No': 0})
-
-# Keep a copy for display (IMPORTANT)
 df_display = df.copy()
 
-# -------------------- ENCODING --------------------
 df_encoded = df.copy()
-
-le_dict = {}  # store encoders for each column
+le_dict ={}  # store encoders for each column
 
 for col in df_encoded.select_dtypes(include='object').columns:
     le = LabelEncoder()
     df_encoded[col] = le.fit_transform(df_encoded[col])
     le_dict[col] = le  # save encoder if needed later
 
-# -------------------- LOAD MODEL --------------------
+
 model = pickle.load(open("attrition_model.pkl", "rb"))
 
-# -------------------- TITLE --------------------
-st.title("🧑‍💼 Employee Attrition Analysis Dashboard")
 
-# -------------------- KPIs --------------------
+st.title("Employee Attrition Analysis Dashboard")
+
+
 total_emp = len(df)
 attrition_rate = df['Attrition'].mean() * 100
 avg_salary = df['MonthlyIncome'].mean()
@@ -48,7 +40,7 @@ c3.metric("Average Monthly Income", f"{avg_salary:.0f}")
 
 st.divider()
 
-# -------------------- VISUALIZATIONS --------------------
+
 col1, col2 = st.columns(2)
 
 with col1:
@@ -66,15 +58,12 @@ with col2:
     st.pyplot(fig)
 
 st.divider()
-
-# -------------------- ATTRITION PREDICTION --------------------
 st.subheader("⚠️ High Risk Employees")
 
 X = df_encoded.drop('Attrition', axis=1)
-
 df_display['Attrition_Probability'] = model.predict_proba(X)[:, 1]
-
 high_risk = df_display[df_display['Attrition_Probability'] > 0.6]
+high_risk = high_risk.reset_index(drop=True)
 
 st.dataframe(
     high_risk[
