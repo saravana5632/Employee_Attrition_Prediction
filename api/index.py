edd89df8656a13235d63836f9ca2935804382c5a
SaravanaKumar G <oatktg.saravana5632@gmail.com>
2026-04-25 17:50:33 +0530

    Refactor file paths for dataset and model loading
    Updated file paths for dataset and model loading to use absolute paths, ensuring compatibility with deployment environments.

--- api/index.py
+++ api/index.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template
 import pandas as pd
 import pickle
 from sklearn.preprocessing import LabelEncoder
+import os
 
 app = Flask(__name__, template_folder='../templates')
 
-# Load dataset
-df = pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
+# Use absolute paths (important on Vercel/Render)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+csv_path = os.path.join(BASE_DIR, "WA_Fn-UseC_-HR-Employee-Attrition.csv")
+model_path = os.path.join(BASE_DIR, "attrition_model.pkl")
+
+# Load data
+df = pd.read_csv(csv_path)
 df['Attrition'] = df['Attrition'].map({'Yes': 1, 'No': 0})
 df_display = df.copy()
 
@@ -14,10 +20,9 @@ df_encoded = df.copy()
 for col in df_encoded.select_dtypes(include='object').columns:
     df_encoded[col] = LabelEncoder().fit_transform(df_encoded[col])
 
-model = pickle.load(open("attrition_model.pkl", "rb"))
+model = pickle.load(open(model_path, "rb"))
 
-
-@app.route('/dashboard')
+@app.route('/')
 def dashboard():
     total_emp = len(df)
     attrition_rate = round(df['Attrition'].mean() * 100, 2)
